66f681ad44212e5ca590886b9adcd6bd51283074
Pavel_143477 <darthpsh@gmail.com>
2021-08-23 04:16:01 +0300

    oops again

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,22 +1,25 @@
-const counCalcPlus = document.querySelectorAll('.cart__prod-count__plus');
-const counCalcMinus = document.querySelectorAll('.cart__prod-count__minus');
+window.addEventListener('DOMContentLoaded', function() {
+    const counCalcPlus = document.querySelectorAll('.cart__prod-count__plus');
+    const counCalcMinus = document.querySelectorAll('.cart__prod-count__minus');
 
-counCalcMinus.forEach(function(item) {
-    item.addEventListener('click', function() {
-        let countMinus = this.nextSibling.innerHTML;
-        if(+countMinus >= 1) {
-            this.nextSibling.innerHTML--;
-        }
+    counCalcMinus.forEach(function(item) {
+        item.addEventListener('click', function() {
+            let countMinus = this.nextSibling.innerHTML;
+            if(+countMinus >= 1) {
+                this.nextSibling.innerHTML--;
+            }
+        })
     })
-})
-counCalcPlus.forEach(function(item) {
-    item.addEventListener('click', function() {
-        this.previousSibling.innerHTML++;
+    counCalcPlus.forEach(function(item) {
+        item.addEventListener('click', function() {
+            this.previousSibling.innerHTML++;
+        })
     })
-})
 
-var swiper = new Swiper(".related__prods", {
-    slidesPerView: 'auto',
-    spaceBetween: 18,
-    freeMode: true,
-});
+    var swiper = new Swiper(".related__prods", {
+        slidesPerView: 'auto',
+        spaceBetween: 18,
+        freeMode: true,
+    });
+
+})
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,25 @@
-"use strict";var counCalcPlus=document.querySelectorAll(".cart__prod-count__plus"),counCalcMinus=document.querySelectorAll(".cart__prod-count__minus");counCalcMinus.forEach(function(e){e.addEventListener("click",function(){1<=+this.nextSibling.innerHTML&&this.nextSibling.innerHTML--})}),counCalcPlus.forEach(function(e){e.addEventListener("click",function(){this.previousSibling.innerHTML++})});var swiper=new Swiper(".related__prods",{slidesPerView:"auto",spaceBetween:18,freeMode:!0});
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function () {
+  var counCalcPlus = document.querySelectorAll('.cart__prod-count__plus');
+  var counCalcMinus = document.querySelectorAll('.cart__prod-count__minus');
+  counCalcMinus.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var countMinus = this.nextSibling.innerHTML;
+
+      if (+countMinus >= 1) {
+        this.nextSibling.innerHTML--;
+      }
+    });
+  });
+  counCalcPlus.forEach(function (item) {
+    item.addEventListener('click', function () {
+      this.previousSibling.innerHTML++;
+    });
+  });
+  var swiper = new Swiper(".related__prods", {
+    slidesPerView: 'auto',
+    spaceBetween: 18,
+    freeMode: true
+  });
+});
